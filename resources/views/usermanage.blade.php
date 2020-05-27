ec010bdd27404abe3fe03cdaf6f0a54f1d5498ba
JoshuaMac-USC <macaldojoshua23@gmail.com>
2020-05-27 18:33:40 +0800

    Update usermanage.blade.php

--- resources/views/usermanage.blade.php
+++ resources/views/usermanage.blade.php
@@ -10,7 +10,7 @@
 @endif
 <!-- Modal -->
 <div class="modal fade" id="ingoing" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
+  <div class="modal-dialog modal-xl">
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="exampleModalLabel">Add User</h5>
